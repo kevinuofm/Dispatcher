9d1ffc3a196d38f63a37434980aaa55df46b437d
Ariel Liu <aliu@lyft.com>
2016-03-14 16:23:59 -0700

    add mapkit

--- DispatchDash/DispatchDash/ViewController.swift
+++ DispatchDash/DispatchDash/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var myMap: MKMapView!
+    @IBOutlet weak var carButtonView: MKAnnotationView!
+    
+    var myRoute : MKRoute?
+    
+    var point1 : MKPointAnnotation!
+    var point2 : MKPointAnnotation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        point1 = MKPointAnnotation()
+        point2 = MKPointAnnotation()
+        
+        point1.coordinate = CLLocationCoordinate2DMake(25.0305, 121.5360)
+        point1.title = "Taipei"
+        point1.subtitle = "Taiwan"
+        myMap.addAnnotation(point1)
+        
+        point2.coordinate = CLLocationCoordinate2DMake(24.9511, 121.2358)
+        point2.title = "Chungli"
+        point2.subtitle = "Taiwan"
+        myMap.addAnnotation(point2)
+        myMap.centerCoordinate = point2.coordinate
+        myMap.delegate = self
+        
+        //Span of the map
+        myMap.setRegion(MKCoordinateRegionMake(point2.coordinate, MKCoordinateSpanMake(0.7,0.7)), animated: true)
+        
+        let directionsRequest = MKDirectionsRequest()
+        let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
+        let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
+        
+        directionsRequest.source = MKMapItem(placemark: markChungli)
+        directionsRequest.destination = MKMapItem(placemark: markTaipei)
+        
+        directionsRequest.transportType = MKDirectionsTransportType.Automobile
+        let directions = MKDirections(request: directionsRequest)
+        
+        directions.calculateDirectionsWithCompletionHandler { (response:MKDirectionsResponse?, error:NSError?) -> Void in
+            if error == nil {
+                self.myRoute = response!.routes[0]
+                self.myMap.addOverlay((self.myRoute?.polyline)!)
+            }
+        }
     }
-
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let myLineRenderer = MKPolylineRenderer(polyline: (myRoute?.polyline)!)
+        myLineRenderer.strokeColor = UIColor.redColor()
+        myLineRenderer.lineWidth = 3
+        return myLineRenderer
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
